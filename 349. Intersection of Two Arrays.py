d10dccbf6abbd569762610e7e1be009aa983f418
ravitejalam <ravitejalam28@gmail.com>
2024-01-25 01:28:56 +0530

    Thu Jan 25 01:28:56 IST 2024

---
+++ 349. Intersection of Two Arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        distinct_nums1 = set(nums1)
+        ans = []
+        for i in nums2:
+            if i in distinct_nums1:
+                ans.append(i)
+                distinct_nums1.remove(i)
+        return ans
